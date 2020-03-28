5aa2c3866acc3cb91162c208a40608eeae5f3ddf
Brendan Laughlin <brendan.laughlin@endurance.com>
2020-03-28 19:31:54 -0400

    began writing basic run function for tree-install module

--- modules/tree_install.py
+++ modules/tree_install.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
-def run():
-    print('running tree-install')
+from subprocess import Popen, STDOUT, PIPE
+
+def run(packageManager, env, dirs):
+    env=('--'+env) if not env else ''
+
+    for dir in dirs:
+        install = Popen([packageManager, 'install', env], STDOUT=PIPE, STDERR=STDOUT)
+        installOut,installErr = install.communicate()
+
+        if installErr:
+            print(installErr)
+        else:
+            print(f"Finished installing in {dir}") 
 
 if __name__ == '__tree_install__':
     run()
