2749b4c065b28be8a8c397e300f0285b0741e483
Brendan Laughlin <brendanwl1538@gmail.com>
2020-05-08 21:57:41 -0400

    pulling aws keys from environment variables now

--- modules/s3.py
+++ modules/s3.py
@@ -3,6 +3,7 @@
 import boto3
 from botocore.exceptions import ClientError
 import os
+import sys
 
 def upload_to_s3(bucket, s3, repoDir, buildOutputFolder):
     # declare objectName and fileName
@@ -17,22 +18,26 @@ def upload_to_s3(bucket, s3, repoDir, buildOutputFolder):
       fileName = file.split(repoDir)[1].lstrip('/')
       try:
         res = s3.upload_file(file, bucket, fileName)
-      except ClientError as err:
-        print(err)
-    print(f'Successfully pushed to {bucket}')
+        print(f'Successfully pushed to {bucket}')
+      except:
+        print(f'Failed to push to {bucket}')
 
 def main_process(processArgs, repoDir, buildOutputFolder):
     bucketName = processArgs['bucket']
-    AWS_ACCESS_KEY_ID = processArgs['access_key']
-    AWS_SECRET_ACCESS_KEY = processArgs['secret_key']
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
+    
+    if AWS_ACCESS_KEY_ID == None or AWS_SECRET_ACCESS_KEY == None:
+      print('AWS_ACCESS_KEY and AWS_SECRET_KEY environment variables need to be set.')
+      sys.exit()
+    else:
+      s3 = boto3.client(
+          's3',
+          aws_access_key_id=AWS_ACCESS_KEY_ID,
+          aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+      )
 
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-    )
+      upload_to_s3(bucketName, s3, repoDir, buildOutputFolder)
 
-    upload_to_s3(bucketName, s3, repoDir, buildOutputFolder)
-
-if __name__ == '__s3__':
+if __name__ == '__main__':
     main_process()
